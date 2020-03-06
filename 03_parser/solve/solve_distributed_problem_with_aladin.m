a8393d5d09aa9d4e8245faf7752b9cb53d2c9fa5
uthgg <xinliang.dai@student.kit.edu>
2020-03-06 13:06:21 +0100

    elapsed_time = totTime -plotTimer, fixes #11

--- 03_parser/solve/solve_distributed_problem_with_aladin.m
+++ 03_parser/solve/solve_distributed_problem_with_aladin.m
@@ -7,8 +7,8 @@ function [xsol, xsol_stacked, mpc_sol] = solve_distributed_problem_with_aladin(m
     [xsol, xsol_stacked] = deal_solution(xsol, mpc, names); 
     
     %% numerical solution back to matpower casefile
-    iter          =  size(sol.iter.logg.X, 1); % number of iteration
-    elapsed_time  =  timers.totTime;
+    iter          =  size(sol.iter.logg.X, 2); % number of iteration
+    elapsed_time  =  timers.totTime - timers.iter.plotTimer;
     alg           =  'ALADIN';
     mpc_sol       =  back_to_mpc(mpc, xsol, elapsed_time, iter, alg);
 end
