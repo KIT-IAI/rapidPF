c4aa531aa74ba2a87069d1decd53e2b296290e44
Tillmann Mühlpfordt <tillmann.muehlpfordt@kit.edu>
2020-03-06 13:24:18 +0100

    moved naming to separate file (#12)

--- 00_use-case/test_file.m
+++ 00_use-case/test_file.m
@@ -1,19 +1,10 @@
-    clear all; close all; clc;
+clear all; close all; clc;
 
 addpath(genpath('../01_generator/'));
 addpath(genpath('../02_splitter/'));
 addpath(genpath('../03_parser/'));
 
-names.regions.global = 'regions';
-names.regions.global_with_copies = 'connections_with_aux_nodes';
-names.regions.local = 'regions_local';
-names.regions.local_with_copies = 'regions_local_with_copies';
-names.copy_buses.local = 'copy_buses_local';
-names.copy_buses.global = 'copy_buses_global';
-names.connections.local = 'connections_global';
-names.connections.global = 'connections';
-names.split = 'split_case_files';
-
+names = generate_name_struct();
 %% setup
 fields_to_merge = { 'bus', 'gen', 'branch' };
 
