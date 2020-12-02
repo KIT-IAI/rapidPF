8d112fa5567c68ee5d8d1e038fa7ad704c5bdfa6
bennerh <henrieke.benner@googlemail.com>
2020-12-02 15:56:27 +0100

    added documentation for inital condition and lower/upper bounds

--- 06_opf_extension/build_local_initial_conditions.m
+++ 06_opf_extension/build_local_initial_conditions.m
@@ -1,3 +1,14 @@
 function x0 = build_local_initial_conditions(om)
-    x0 = om.params_var();
+% BUILD_LOACL_INITIAL_CONDITION
+%
+%   `x0 = build_local_initial_conditions(om)`
+%
+%   _extracts initial condition for x from MATPOWER_
+%
+%   INPUT:
+%   - $\texttt{om}$ optimization model of reduced splitted case file
+%
+%  OUTPUT:
+%  - $\texttt{x0}$ initial condition for objective variable $x$ of model $\texttt{om}$ of subsystem $i$    
+x0 = om.params_var();
 end
