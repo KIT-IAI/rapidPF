e311a47fef7d4c69d2f017eff7cc3bbffb96be68
bennerh <henrieke.benner@googlemail.com>
2020-12-27 10:15:07 +0100

    found and corected error in Lagrangian function

--- 06_opf_extension/build_local_lagrangian_function.m
+++ 06_opf_extension/build_local_lagrangian_function.m
@@ -7,16 +7,16 @@ function [L, dLdx, d2Ldx] = build_local_lagrangian_function(f, df, g, dg, h, dh)
 % dL = grad_f + lambda'(Jac_g)' + mu'(Jac_h)'
 
     if nargin == 4
-        L = @(x, kappa, Neq) f(x) + kappa(1:Neq)'*g(x);
-        dLdx = @(x, kappa, Neq) df(x) ...
-            + get_grad_lambda_g(kappa(1:Neq), dg, x);
-        d2Ldx = @(x, kappa, rho, Neq)  get_Hess(dLdx, x, kappa, Neq);
+        L = @(x, kappa) f(x) + kappa(1:length(g(x)))'*g(x);
+        dLdx = @(x, kappa) df(x) ...
+            + get_grad_lambda_g(kappa(1:length(g(x))), dg);
+        d2Ldx = @(x, kappa, rho)  get_Hess(dLdx, x, kappa);
     elseif nargin == 6
-        L = @(x, kappa, Neq) f(x) + kappa(1:Neq)'*g(x) + kappa(Neq+1:end)'*h(x);
-        dLdx = @(x, kappa, Neq) df(x)  + ...
-            get_grad_lambda_g(kappa(1:Neq), dg, x) + ...
-            get_grad_mu_h(kappa(Neq+1:end), dh, x);
-        d2Ldx = @(x, kappa, rho, Neq) get_Hess(dLdx, x, kappa, Neq);
+        L = @(x, kappa) f(x) + kappa(1:length(g(x)))'*g(x) + kappa(length(g(x))+1:end)'*h(x);
+        dLdx = @(x, kappa) df(x)  + ...
+            get_grad_lambda_g(kappa(1:length(g(x))), dg, x) + ...
+            get_grad_mu_h(kappa(length(g(x))+1:end), dh, x);
+        d2Ldx = @(x, kappa, rho) get_Hess(dLdx, x, kappa);
     end
 
 
@@ -46,14 +46,14 @@ function grad_mu_h = get_grad_mu_h(mu, dh, x)
 end
 
 %% get Hessian Function
-function d2Ldx = get_Hess(dLdx, x, kappa, Neq)
+function d2Ldx = get_Hess(dLdx, x, kappa)
 epsilon = 1e-10;
 epsilon_inv = 1/epsilon;
 nx = length(x);
 d2Ldx = zeros(nx, nx);
 for i = 1 : nx
     dx_i = [ zeros(i-1, 1); 1; zeros(nx-i, 1)];
-    d2Ldx(:, i) = (dLdx(x + epsilon*dx_i, kappa, Neq) ...
-        - dLdx(x - epsilon*dx_i, kappa, Neq)).*0.5.*epsilon_inv;
+    d2Ldx(:, i) = (dLdx(x + epsilon*dx_i, kappa) ...
+        - dLdx(x - epsilon*dx_i, kappa)).*0.5.*epsilon_inv;
 end
 end
