596439ed9b5b792b70beb24ce4ea531c0afcd5de
bennerh <henrieke.benner@googlemail.com>
2021-05-04 15:48:29 +0200

    corrected error

--- 06_opf_extension/get_local_variable_from_global_objective_variable.m
+++ 06_opf_extension/get_local_variable_from_global_objective_variable.m
@@ -2,6 +2,8 @@ function x_local = get_local_variable_from_global_objective_variable(x_global, i
 % get local objective variable of subsystem i from global variable of
 % merged system.
 
+% To be debugged!!!
+
 N_subsystems = length(mpc_split.regions);
 
 assert(i_subsystem <= N_subsystems, 'index of subsystem is larger then total number of subsystem');
@@ -21,34 +23,13 @@ V_local                 = zeros(1, length(mpc_split.connections_with_aux_nodes{i
 Pg_local                = zeros(1, N_gens{i_subsystem});
 Qg_local                = zeros(1, N_gens{i_subsystem});
 
-
-nodes_before          = 0;
 gens_before           = 0;
 for i = 1 : i_subsystem - 1
-    nodes_before = nodes_before + length(mpc_split.regions{i});
     gens_before  = gens_before + N_gens{i};
 end
 
-% get indices corresponding to theta
-for i = 1 : length(mpc_split.regions{i_subsystem})
-    theta_local(i) = x_global(i + nodes_before);
-end
-
-for i = 1 : length(mpc_split.copy_buses_global(i_subsystem))
-    copy_buses = mpc_split.copy_buses_global(i_subsystem);
-    theta_local(length(mpc_split.regions{i_subsystem}) + i) = x_global(copy_buses{1}(i));
-end
-
-% get indices corresponding to Vm
-for i = 1 : length(mpc_split.regions{i_subsystem})
-    V_local(i) = x_global(i + nodes_before + total_number_of_nodes);
-end
-
-for i = 1 : length(mpc_split.copy_buses_global(i_subsystem))
-    copy_buses_global = mpc_split.copy_buses_global(i_subsystem);
-    V_local(length(mpc_split.regions{i_subsystem}) + i) = x_global(copy_buses{1}(i) + total_number_of_nodes);
-end
-
+theta_local = x_global(mpc_split.connections_with_aux_nodes{i_subsystem})';
+V_local = x_global(mpc_split.connections_with_aux_nodes{i_subsystem} + total_number_of_nodes)';
 
 % get indices corresponding to Pg
 for i = 1 : length(Pg_local)
